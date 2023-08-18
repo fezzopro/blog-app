bc99249393e4dbbb8eb0842833bbb7aec6eb20d9
Fezzopro <fezzopro@gmail.com>
2023-08-18 16:27:04 +0200

    Correct linters erors

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :users, class_name: 'User'
-  has_many :comments, class_name: "Comment", foreign_key: "post_id"
+  has_many :comments, class_name: 'Comment', foreign_key: 'comment_id'
 end
