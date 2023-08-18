50915245b33af9c084e3fd34783858b2a57930f6
Fezzopro <fezzopro@gmail.com>
2023-08-18 16:38:33 +0200

    Add created Like model using rails g model command

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :users, class_name: 'User'
-  has_many :comments, class_name: 'Comment', foreign_key: 'comment_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'post_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'author_id'
 end
