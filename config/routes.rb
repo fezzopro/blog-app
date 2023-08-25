156386c3c5afc8daecd0155ba98577be46743826
Fezzopro <fezzopro@gmail.com>
2023-08-25 23:11:27 +0200

    Add users and posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/index'
+  get '/users/:user_id/posts', to: 'posts#index'
+  get '/users/:user_id/posts/:id', to: 'posts#show'
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "pages#index"
+  # Defines the root path route ('/')
+  root 'pages#index'
 end
