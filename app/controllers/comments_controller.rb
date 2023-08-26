e3695341f0ae987725b62fd071170dcd11e4deaf
Fezzopro <fezzopro@gmail.com>
2023-08-26 15:48:47 +0200

    Add CommentsController to the blog

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    Rails.logger.debug(params.inspect)
+    @post = Post.find(params[:post_id])
+    @comment = @current_user.comments.new(params.permit(:Text))
+    @comment.post = @post
+
+    if @comment.save
+      redirect_to user_post_path(user_id: @current_user.id, id: @post.id),
+                  notice: 'Comment added successfully.'
+    else
+      render :new
+    end
+  end
+end
