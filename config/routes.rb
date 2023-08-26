125b8fd36f95e7cb448af1df6ae1f4590926fd66
Fezzopro <fezzopro@gmail.com>
2023-08-26 15:51:34 +0200

    Add comment and likes routes to the blog application

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/users/:user_id/posts/:id', to: 'posts#show'
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
+  resources :comments, only: [:new, :create]
+  resources :likes, only: [:create, :destroy]
+  get 'likes', to: 'likes#like', on: :member
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ('/')
