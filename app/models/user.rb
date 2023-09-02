4e90299ca982fbafff1907b1e93aeedd760dd4f9
Fezzopro <fezzopro@gmail.com>
2023-09-02 17:03:47 +0200

    Generate devise using rails CLI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  before_validation :set_initial_post_counter, on: :create
+
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :validatable
+
   has_many :posts, class_name: 'Post', foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: 'author_id'
   has_many :likes, class_name: 'Like', foreign_key: 'author_id'
@@ -9,4 +14,10 @@ class User < ApplicationRecord
   def recent_posts(limit = 3)
     posts.order(created_at: 'desc').limit(limit)
   end
+
+  def set_initial_post_counter
+    self.post_counter = 0
+  end
+
+  private :set_initial_post_counter
 end
